f076659dc95fae50782539b5bb6b962971b6884d
ladla18 <amanullahansari306@gmail.com>
2024-11-12 01:13:00 +0530

    added pricing

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -54,7 +54,12 @@ const Navbar: React.FC = () => {
     <>
       <div className="bg-white text-black md:w-2/4 w-11/12 md:mx-2 mx-5 rounded-2xl flex border border-black justify-between items-center px-5 py-2 bottom-6 md:inset-x-1/4 inset-x-auto fixed z-10">
         <div className="flex items-center space-x-4">
-          <Image src="/image2.jpg" alt="Logo" width={30} height={30} />
+          <Image
+            src="/WhatsApp Image 2024-11-12 at 01.05.28_9a322af3.jpg"
+            alt="Logo"
+            width={30}
+            height={30}
+          />
           <Link
             className="hover:underline hidden md:inline-block "
             href="/pricing"
